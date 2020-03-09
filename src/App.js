b421d7933eb06136936d4053482b1ebc690248c0
bryan_v <bryan.villafuerte@bryvil.com>
2020-03-09 23:08:39 +0100

    created app, added required dependencies, and created first component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from "./components/layout/Heading";
+import './sass/styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+    const title = "JS FRAMEWORKS MA3";
+    const subtitle = "React Appgit ";
+
+	return (
+        <div>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+        </div>
+    )
+}
+
+export default Heading;
